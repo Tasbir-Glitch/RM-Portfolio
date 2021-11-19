2cbee299d5632ecdc99dd5f29fc1de8cd40a9363
Tasbir-Glitch <82874560+Tasbir-Glitch@users.noreply.github.com>
2021-11-19 23:31:27 +0600

    Add files via upload

---
+++ js/isotope-main.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+
+
+  $(document).ready(function(){
+    $('.portfolio-item').isotope(function(){
+        itemSelector:'.item'
+      });
+  
+  
+  
+    $('.portfolio-menu ul li').click(function(){
+      $('.portfolio-menu ul li').removeClass('active');
+      $(this).addClass('active');
+  
+  
+      var selector = $(this).attr('data-filter');
+        $('.portfolio-item').isotope({
+          filter: selector
+        })
+        return false;
+    });
+  });
+
+})(jQuery);
